b65a27c826e937e759aacbacac09bdf93fcbaa17
OlejaPythonist <125196845+OlejaPythonist@users.noreply.github.com>
2023-03-26 16:35:21 +0000

    Add files via upload
    Создание кастомных ошибок

---
+++ JSONDecoder/exceptions.py
@@ -0,0 +1,18 @@
+class IllegalCharacter(Exception):
+    pass
+
+
+class EOFErrorJSON(Exception):
+    pass
+
+
+class InvalidJSONSyntax(Exception):
+    pass
+
+
+class InvalidKey(Exception):
+    pass
+
+
+class KeyDuplicate(Exception):
+    pass
